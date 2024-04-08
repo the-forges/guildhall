4f0acd45e79d52dd7ddd6654db697958f70ea42b
Michael Christenson II <m3talsmith@gmail.com>
2024-04-08 13:31:11 -0400

    Enough to test deployment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{get, routes};
+use rocket::fs::FileServer;
+
+#[get("/")]
+fn hello() -> String {
+    "Hello, world!".to_string()
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    rocket::build()
+        .mount("/hello", routes![hello])
+        .mount("/", FileServer::from("./static"))
+        .launch()
+        .await?;
+    Ok(())
 }
