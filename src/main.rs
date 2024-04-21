f208affaf5928dba88bbe7c8722a9da377402147
Michael Christenson II <m3talsmith@gmail.com>
2024-04-21 17:39:28 -0400

    Added cors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::env;
+use std::error::Error;
 use rocket::{routes};
+use rocket::http::Method;
 use rocket::fs::FileServer;
+use rocket_cors::{AllowedHeaders, AllowedOrigins};
 
 mod api;
 mod database;
@@ -8,12 +11,26 @@ mod models;
 mod schema;
 
 #[rocket::main]
-async fn main() -> Result<(), rocket::Error> {
+async fn main() -> Result<(), Box<dyn Error>> {
     let _ = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let allowed_origins = AllowedOrigins::all();
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        allow_credentials: true,
+        ..Default::default()
+    }
+        .to_cors()?;
 
     rocket::build()
-        .mount("/api", routes![api::authentication::preauth, api::authentication::authenticate, api::authentication::authenticated])
+        .mount("/api", routes![
+            api::authentication::preauth,
+            api::authentication::authenticate,
+            api::authentication::authenticated,
+        ])
         .mount("/", FileServer::from("./static"))
+        .attach(cors)
         .launch()
         .await?;
     Ok(())
